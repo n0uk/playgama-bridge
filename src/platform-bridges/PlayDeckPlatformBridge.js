bfae7829df09167d6f8efbe6930825329d4243c1
Ivan Lysov <ivan.lysov@gmail.com>
2024-10-21 14:46:18 +0500

    playdeck game_ready message; vk leaderboard issupported false

--- src/platform-bridges/PlayDeckPlatformBridge.js
+++ src/platform-bridges/PlayDeckPlatformBridge.js
@@ -91,6 +91,7 @@ class PlayDeckPlatformBridge extends PlatformBridgeBase {
 
                     this._isInitialized = true
                     this._resolvePromiseDecorator(ACTION_NAME.INITIALIZE)
+                    this.sendMessage(PLATFORM_MESSAGE.GAME_READY)
                 }
             }
 
